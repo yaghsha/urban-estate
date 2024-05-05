f79feabd94eeb3277115aad8c19ec95ac6e12c29
urban estate <waseyaghsha@gmail.com>
2024-05-05 16:06:33 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUP';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUP" element={<SignUp />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Profile" element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }

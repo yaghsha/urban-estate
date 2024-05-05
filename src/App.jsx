e4d3541d8ac4b87de3e72e0b94806c3ac622c26a
urban estate <waseyaghsha@gmail.com>
2024-05-05 17:21:56 +0500

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUP';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './Components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/SignIn" element={<SignIn />} />
